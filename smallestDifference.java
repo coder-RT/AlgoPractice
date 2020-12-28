0b33d66fb28aec8b39ee43e21ea24fe5def4b129
Rachit <rachit2311@gmail.com>
2020-12-28 10:53:31 +0530

    SmallestDifference - initial commit

---
+++ smallestDifference.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class smallestDifference {
+    public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo){
+        return new int[] {};
+    }
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(smallestDifference(new int[] {-1,5,10,20,28,3}, new int[] {26,134,135,15,17})));
+    }
+}
