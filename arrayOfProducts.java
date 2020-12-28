78fe1af05c11f255a23f6bd1cf59e93ce166cab5
Rachit <rachit2311@gmail.com>
2020-12-28 12:08:34 +0530

    arrayOfProucts : initial commit

---
+++ arrayOfProducts.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class arrayOfProducts {
+    public static int[] arrayOfProducts(int[] array){
+        return new int[] {};
+    }
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(arrayOfProducts(new int[] {5,1,4,2})));
+    }
+}
