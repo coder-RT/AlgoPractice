6006cdfb33c3577d2c781aea3e4bacef3195b844
Rachit <rachit2311@gmail.com>
2021-01-09 02:40:33 +0530

    add binary | initial commit

---
+++ binaryString.java
@@ -0,0 +1,9 @@
+public class binaryString {
+    public static String addBinary(String A, String B) {
+        return "";
+    }
+    public static void main(String[] args){
+        String res = addBinary("100", "11");
+        System.out.println(res);
+    }
+}
