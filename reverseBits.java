adbab0275ec9ee9780f9f9a92ada1257af1793b4
Rachit <rachit2311@gmail.com>
2021-01-08 17:59:49 +0530

    reverseBits initial commit

---
+++ reverseBits.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class reverseBits {
+    public static int reverse(int A){
+        return 0;
+    }
+    public static void main(String[] args){
+        int res = reverse(3);
+        System.out.println(res);
+    }
+}
