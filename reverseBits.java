f24db5294ccf62c1b3dd13f4fdff267a624ce24c
Rachit <rachit2311@gmail.com>
2021-01-08 18:37:10 +0530

    reverseBits

--- reverseBits.java
+++ reverseBits.java
@@ -1,11 +1,28 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class reverseBits {
-    public static int reverse(int A){
-        return 0;
+    public static long reverse(long A){
+        int bits = 32;
+        long result = 0;
+        long[] lst = new long[bits];
+
+        int index = 0;
+        while(bits > 0){
+            lst[index] = A & 1;
+            A = A >> 1;
+            index++;
+            bits--;
+        }
+
+        for(int i=0; i< lst.length; i++){
+            result = result << 1;
+            result = result | lst[i];
+        }
+        return result;
     }
     public static void main(String[] args){
-        int res = reverse(3);
+        long res = reverse(3);
         System.out.println(res);
     }
 }
